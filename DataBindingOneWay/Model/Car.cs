aa4d843dc0e5e40e60d7dd48e3954c5b5b4e3480
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-11-01 10:17:39 +0100

    One Way Data Binding Project

---
+++ DataBindingOneWay/Model/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace DataBindingOneWay.Model
+{
+   public class Car
+   {
+       public int Id { get; set; }
+       public string Name { get; set; }
+
+       public Car(int id, string name)
+       {
+           Id = id;
+           Name = name;
+       }
+       public override string ToString()
+       {
+           return $"ID:{Id}-Name:{Name}";
+       }
+   }
+}
---
+++ DataBindingOneWay/ViewModel/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿
+using DataBindingOneWay.Model;
+
+namespace DataBindingOneWay.ViewModel
+{
+    public class ViewModel
+    {
+        // Source property 
+        public Car Bmw { get; set; }
+        public Car Audi { get; set; }
+
+        public ViewModel()
+        {
+            Bmw = new Car(12,"BMW");
+            Audi = new Car(23, "Audi");
+        }
+
+    }
+}
