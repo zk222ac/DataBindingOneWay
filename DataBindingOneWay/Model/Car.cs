2b3416a930d39e50f546416cb24170bf599572e8
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-11-01 11:30:11 +0100

    Finally Add attributes

--- DataBindingOneWay/Model/Car.cs
+++ DataBindingOneWay/Model/Car.cs
@@ -2,17 +2,24 @@
 {
    public class Car
    {
-       public int Id { get; set; }
-       public string Name { get; set; }
+       private int _id;
+       private string _name;
+       public int Id
+       {
+           get { return _id; }
+           set { _id = value; }
+       }
+       public string Name
+       {
+           get { return _name; }
+           set { _name = value; }
+       }
 
        public Car(int id, string name)
        {
            Id = id;
            Name = name;
        }
-       public override string ToString()
-       {
-           return $"ID:{Id}-Name:{Name}";
-       }
+
    }
 }
